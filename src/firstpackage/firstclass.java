c8a6579a2c078f6df6b3aef011c0b2627b76cbbb
Ali Atilgan <a.atilgan1720@gmail.com>
2021-03-05 23:21:38 -0600

    third commit from your local master

--- src/firstpackage/firstclass.java
+++ src/firstpackage/firstclass.java
@@ -5,6 +5,7 @@ public class firstclass {
 	public static void main(String[] args) {
 		System.out.println("First commit from local master branch");
 		System.out.println("Second commit from local master");
+		System.out.println("third commit from local master");
 
 	}
 
